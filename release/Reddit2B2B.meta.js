fd2a29fc2f7350cc87293e518e0a2f2b3790c94f
s0uboxxx <s0uboxxx@users.noreply.github.com>
2026-09-15 12:31:28 +0000

    release

---
+++ release/Reddit2B2B.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name             Reddit++
+// @name:ru          Reddit++
+// @description      Upgrade your Reddit experience: remove annoying elements, make the UI cleaner and user-friendly, and enjoy powerful features like keyword filtering — highly customizable to fit your needs.
+// @description:ru   Множество улучшений Reddit: удаление раздражающих элементов, более удобный интерфейс и продвинутые функции вроде фильтрации по ключевым словам с гибкой настройкой под ваши нужды.
+// @namespace        RedditPlusPlus
+// @version          2.1.6
+// @author           lnm95
+// @icon             https://raw.githubusercontent.com/lnm95/redditPlusPlus/main/public/icon/redditPlusPlusIcon.png
+// @source           https://github.com/lnm95/redditPlusPlus
+// @license          MIT
+// @match            *://*.reddit.com/*
+// @grant            unsafeWindow
+// @grant            GM_getValue
+// @grant            GM_setValue
+// @grant            GM_deleteValue
+// @run-at           document-start
+// @downloadURL https://raw.githubusercontent.com/s0uboxxx/Userscripts-GCC/release/release/Reddit2B2B.user.js
+// @updateURL https://raw.githubusercontent.com/s0uboxxx/Userscripts-GCC/release/release/Reddit2B2B.meta.js
+// ==/UserScript==
---
+++ release/YouTube20JS20Engine20Tamer.meta.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name        YouTube JS Engine Tamer
+// @name:ja     YouTube JS Engine Tamer
+// @name:zh-TW  YouTube JS Engine Tamer
+// @name:zh-CN  YouTube JS Engine Tamer
+// @namespace   UserScripts
+// @version     0.42.25
+// @match       https://www.youtube.com/*
+// @match       https://www.youtube-nocookie.com/embed/*
+// @match       https://studio.youtube.com/live_chat*
+// @license     MIT
+// @author      CY Fung
+// @icon        https://raw.githubusercontent.com/cyfung1031/userscript-supports/main/icons/yt-engine.png
+// @grant       none
+// @require     https://cdn.jsdelivr.net/gh/cyfung1031/userscript-supports@c2b707e4977f77792042d4a5015fb188aae4772e/library/nextBrowserTick.min.js
+// @run-at      document-start
+// @unwrap
+// @inject-into page
+// @allFrames   true
+// @exclude     /^https?://\S+\.(txt|png|jpg|jpeg|gif|xml|svg|manifest|log|ini)[^\/]*$/
+//
+// @description         To enhance YouTube performance by modifying YouTube JS Engine
+// @description:ja      YouTubeのJSエンジンを変更してパフォーマンスを向上させる
+// @description:zh-TW   修改 YouTube 的 JS 引擎以提升效能
+// @description:zh-CN   修改 YouTube 的 JS 引擎以提升性能
+//
+// @downloadURL https://raw.githubusercontent.com/s0uboxxx/Userscripts-GCC/release/release/YouTube20JS20Engine20Tamer.user.js
+// @updateURL https://raw.githubusercontent.com/s0uboxxx/Userscripts-GCC/release/release/YouTube20JS20Engine20Tamer.meta.js
+// ==/UserScript==
